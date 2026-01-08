77afacb23c02cc1007d1463d31e888afd0994e58
flexult <touma.leandre.72@gmail.com>
2026-01-08 14:37:59 +0000

    début

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phaser Space Game</title>
+</head>
+<body>
+    Salut, ca marche.
+</body>
+</html>
